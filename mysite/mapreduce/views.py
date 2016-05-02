9115fa879cb83867da91ce3db27bb104532d44ef
unknown <yt8mn@OAK.cs.virginia.edu>
2016-05-02 14:06:43 -0400

    html files rename

--- mysite/mapreduce/views.py
+++ mysite/mapreduce/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 def index(request):
-    return render_to_response('templates.html') 
+    return render_to_response('get_job_page.html') 
 def manage(request):
-    return render_to_response('templates1.html') 
+    return render_to_response('job_management_page.html') 
